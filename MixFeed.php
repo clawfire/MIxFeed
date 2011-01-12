efa14ac9024c8e10cdbc2f9d8c260023a42a8289
thibault milan <clawfire@gmail.com>
2011-01-13 00:14:43 +0100

    mixing rss, caching it and serve a mixed one feed.

--- MixFeed.php
+++ MixFeed.php
@@ -1,11 +1,48 @@
 <?php
+/* Read the config file */
 $config = parse_ini_file("config.ini");
-require_once(./simplepie.inc);
-foreach ($config[feeds] as $url){
-	$feed=new SimplePie();
-	$feed->set_feed_url($url);
-	$feed->init();
-	$feed->handle_content_type();
-}
-//print_r($content);
+/* Load rss library */
+require_once('./simplepie.inc');
+/* Load Rss feeds */
+$feeds = new SImplePie($config[feeds]);
+/* Set the per feed item limit */
+$feeds->set_item_limit($config[limit_per_feed]);
+/* Set the cache time */
+$feeds->set_cache_duration ($config[caching_time]);
+/* Enable Direct render outpout (without sanitize and cleaning) */
+$feeds->enable_xml_dump(isset($_GET['xmldump']) ? true : false);
+/* Init Feeds Grabbing */
+$loaded=$feeds->init();
+/* Ensure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it). */
+$feeds->handle_content_type();
+
+/* Starting render a feed */
+echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
+<rss version="2.00">
+<channel>
+<title>Les infos du réseau Tout-Metz.com</title>
+<link>http://www.tout-metz.com</link>
+<description>
+Put your Description Here
+</description>
+<language>fr-fr</language>
+<?php if ($loaded) {
+	$itemlimit=0;
+	foreach($feeds->get_items() as $item) {
+		if ($itemlimit==$config[max_items]) { break; }
+?>
+	<item>
+	<title><?php echo $item->get_title(); ?></title>
+	<link><?php echo $item->get_permalink(); ?></link>
+	<description>
+		<?php echo $item->get_description(); ?>
+	</description>
+	</item>
+<?
+	$itemlimit = $itemlimit + 1;
+	}
+}
+?>
+</channel>
+</rss>
