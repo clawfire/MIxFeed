4ac4191741dbb017a25038d2310519fe669f1f25
thibault milan <clawfire@gmail.com>
2011-01-12 23:41:20 +0100

    trying using SImplePie library

--- MixFeed.php
+++ MixFeed.php
@@ -1,10 +1,11 @@
 <?php
 $config = parse_ini_file("config.ini");
+require_once(./simplepie.inc);
 foreach ($config[feeds] as $url){
-	$feed = simplexml_load_file($url);
-	foreach($feed->channel->item as $item){
-		$content[]=array('date'=>date_format(date_create_from_format('r',$item->pubDate),'U'),'title'=>$item->title,'link'=>$item->link,'description'=>$item->description);
-	}
+	$feed=new SimplePie();
+	$feed->set_feed_url($url);
+	$feed->init();
+	$feed->handle_content_type();
 }
-print_r($content);
+//print_r($content);
 ?>
